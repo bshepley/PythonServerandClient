ac38d410f670f8b4e2c33be6a68bc7f8d6a80580
Brett Shepley <67429536+BrettShepley11@users.noreply.github.com>
2020-12-03 01:20:53 -0500

    Update CommandServer.py

--- CommandServer.py
+++ CommandServer.py
@@ -11,7 +11,6 @@ class Server(object):
   CHECKJOBTEAM <CREATORNAME> <JOBTYPE>
   JOINJOB <CREATORNAME> <JOBTYPE> <SEEKERNAME>
   STARTJOB <CREATORNAME> <JOBTYPE>
-  VIEWACTIVEJOBS <SEEKERNAME>
   
   PREDEFINED VARIABLES
   JOBTYPE->
@@ -26,7 +25,7 @@ class Server(object):
   connectionMessage = ["CREATEJOB  <CREATORNAME> <JOBTYPE> <NUMOFSEEKERS> <TARGETIP> <TARGETPORT>",
                       "REMOVEJOB  <CREATORNAME> <JOBTYPE> <NUMOFSEEKERS> <TARGETIP> <TARGETPORT>",
                       "VIEWJOBS", "CHECKJOBTEAM <CREATORNAME> <JOBTYPE>", "JOINJOB <CREATORNAME> <JOBTYPE> <SEEKERNAME>",
-                      "STARTJOB <CREATORNAME> <JOBTYPE>", "VIEWACTIVEJOBS <SEEKERNAME>", "JOBTYPE SELECTION:", 
+                      "STARTJOB <CREATORNAME> <JOBTYPE>", "JOBTYPE SELECTION:", 
                       "<IP Online Detection>", "<Subnet IP Online Detection>", "<Specific Port Status Detection>",
                       "<All Port Status Detection>", "<ICMP Flood Attack>", "<TCP Flood Attack>", "<UDP Flood Attack>"]
   
@@ -42,21 +41,48 @@ class Server(object):
     self.parameterList = Command.split(" ")
     
   def createJob(self, parameterList):
+    self.connection.send(pickle.dumps("Job has been created and added to the Job List"))
     self.jobListOBJ.createJob(parameterList[1], parameterList[2], parameterList[3], parameterList[4], parameterList[5])
   
   def removeJob(self, parameterList):
     for Job in self.jobListOBJ.listofjobs:
-      if Job.fullJob == parameterList:
-        self.connection.send(pickle.dumps("Job Removed"))
+      if Job.jobParameters == parameterList:
+        self.connection.send(pickle.dumps(Job.FullJob+" has been removed from the Job List"))
         self.jobListOBJ.listofjobs.remove(Job)
-
-  
+        
   def viewJobs(self):
+    self.connection.send(pickle.dumps(jobListOBJ.listofjobs))
   
   def checkJobTeam(self, parameterList):
+    for Job in self.jobListOBJ.listofjobs:
+      if Job.jobParameters == parameterList:
+        self.connection.send(pickle.dumps(Job.JobSeekerList))
+        
   
-  def joinJob(self, parameterList):
+  def joinJob(self, parameterList, seekerName):
+    for Job in self.jobListOBJ.listofjobs:
+      if Job.jobParameters == parameterList:
+        self.connection.send(pickle.dumps(seekerName+" has joined: "+Job.FullName))
+        Job.JobSeekerList.add(seekerName)
+        
   
   def startJob(self, parameterList):
+    for Job in self.jobListOBJ.listofjobs:
+      if Job.jobParameters == parameterList:
+        Job.setNumOfSeekers("Job Started")
   
-  def viewActiveJobs(self):
+  def commandRouting(parameterList):
+    if parameterList[0] == "CREATEJOB":
+      self.createJob()  
+    elif parameterList[0] == "REMOVEJOB":
+      self.removeJob()
+    elif parameterList[0] == "VIEWJOBS":
+      self.viewJob()
+    elif parameterList[0] == "CHECKJOBTEAM":
+      self.checkJobTeam()
+    elif parameterList[0] == "JOINJOB":
+      self.joinJob()
+    elif parameterList[0] == "STARTJOB":
+      self.startJob()
+    else:
+      self.connection.send(pickle.dumps("Invalid Command"))
